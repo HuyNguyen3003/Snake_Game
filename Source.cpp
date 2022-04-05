063075ea17b76e01b7c34a2a0165cb8562bf4b32
Nguyễn Ngọc Huy <94001149+HuyNguyen3003@users.noreply.github.com>
2022-04-05 13:47:01 +0700

    up code
    sửa lại gi cái phần thư viện với cho di chuyển bằng wasd.

--- Source.cpp
+++ Source.cpp
@@ -1,72 +1,5 @@
-#include <stdio.h> 
-#include <conio.h> 
-#include<ctime> 
-#include <windows.h>
-#include <stdint.h>
-#define KEY_NONE -1;
-//======= lấy tọa độ x của con trỏ hiện tại =============
-int whereX(){
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-		return csbi.dwCursorPosition.X;
-	return -1;
-}
-//========= lấy tọa độ y của con trỏ hiện tại =======
-int whereY(){
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
-		return csbi.dwCursorPosition.Y;
-	return -1;
-}
-//============== dịch con trỏ hiện tại đến điểm có tọa độ (x,y) ==========
-void gotoXY(int x, int y){
-	HANDLE hConsoleOutput;
-	COORD Cursor_an_Pos = { x, y };
-	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
-}
-//============= đặt màu cho chữ =========
-void SetColor(WORD color){
-	HANDLE hConsoleOutput;
-	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
-	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
-
-	WORD wAttributes = screen_buffer_info.wAttributes;
-	color &= 0x000f;
-	wAttributes &= 0xfff0;
-	wAttributes |= color;
-
-	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
-}
-//============== làm ẩn trỏ chuột ===========
-void ShowCur(bool CursorVisibility){
-	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
-	SetConsoleCursorInfo(handle, &cursor);
-}
-//======= trả về mã phím người dùng bấm =========
-int inputKey(){
-	if (_kbhit()) { //true
-		int key = _getch();
-		if (key == 224){
-			key = _getch();
-			return key + 1000;
-		}
-		return key;
-	}
-	else{
-		return KEY_NONE;
-	}
-	return KEY_NONE;
-}
-//======= chỉnh màu =========
-void textcolor(int x){
-	HANDLE mau;
-	mau = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(mau, x);
-}
+#define _CRT_SECURE_NO_WARNINGS
+#include "mylib.h"
  FILE* d;
 int tdx[100] = { 0 }, tdy[100] = { 0 };
 int sl = 4;
@@ -332,6 +265,22 @@ void play() {
 				else if ((c == 77 || c == 100) && check != 3){ // phai
 					check = 2;
 				}
+			}
+			else if (c == 115 && check != 0)
+			{
+				check = 1;
+			}
+			else if (c == 119 && check != 1)
+			{
+				check = 0;
+			}
+			else if (c == 97 && check != 2)
+			{
+				check = 3;
+			}
+			else if (c == 100 && check != 3)
+			{
+				check = 2;
 			}
 		}
 		if (check == 1){
