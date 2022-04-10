9c7c0a43bdfdfeaf1d5b697de789b4a211e79a58
Nguyễn Ngọc Huy <94001149+HuyNguyen3003@users.noreply.github.com>
2022-04-10 09:45:52 +0700

    up
    cập nhập lại thư viện

--- mylib.h
+++ mylib.h
@@ -1,37 +1,30 @@
-﻿#pragma once // tránh đụng độ thư viện khi gọi chồng file lên nhau
-#include <stdio.h> // thu vien c
-#include <conio.h> // thư viên trình biên dịch hệ điều hanh cũ
-#include<ctime> /* thư viện hỗ trợ về thời gian thực */
-#include "windows.h" // thư viện này bá đạo lắm nhé - chứa nhiều đồ chơi nek - cứ tìm hiểu dần dần s
-#include <stdint.h>
+#include <stdio.h> 
+#include <conio.h> 
+#include<ctime> 
+#include <windows.h>
 //======= lấy tọa độ x của con trỏ hiện tại =============
-#define KEY_NONE	-1
-int whereX()
-{
+int whereX() {
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
 		return csbi.dwCursorPosition.X;
 	return -1;
 }
 //========= lấy tọa độ y của con trỏ hiện tại =======
-int whereY()
-{
+int whereY() {
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
 		return csbi.dwCursorPosition.Y;
 	return -1;
 }
 //============== dịch con trỏ hiện tại đến điểm có tọa độ (x,y) ==========
-void gotoXY(int x, int y)
-{
+void gotoXY(int x, int y) {
 	HANDLE hConsoleOutput;
 	COORD Cursor_an_Pos = { x, y };
 	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
 }
 //============= đặt màu cho chữ =========
-void SetColor(WORD color)
-{
+void SetColor(WORD color) {
 	HANDLE hConsoleOutput;
 	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -46,36 +39,13 @@ void SetColor(WORD color)
 	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
 //============== làm ẩn trỏ chuột ===========
-void ShowCur(bool CursorVisibility)
-{
+void ShowCur(bool CursorVisibility) {
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
 	SetConsoleCursorInfo(handle, &cursor);
 }
-//======= trả về mã phím người dùng bấm =========
-int inputKey()
-{
-	if (_kbhit()) //true
-	{
-		int key = _getch();
-
-		if (key == 224)
-		{
-			key = _getch();
-			return key + 1000;
-		}
-
-		return key;
-	}
-	else
-	{
-		return KEY_NONE;
-	}
-	return KEY_NONE;
-}
 //======= chỉnh màu =========
-void textcolor(int x)
-{
+void textcolor(int x) {
 	HANDLE mau;
 	mau = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(mau, x);
